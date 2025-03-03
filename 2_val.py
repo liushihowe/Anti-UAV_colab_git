f56087aaf269c14b82965f4debc936f5e9b45afe
到底留什么好 <whenwhenword@outlook.com>
2025-03-03 21:00:44 +0800

    Update 2_val.py

--- 2_val.py
+++ 2_val.py
@@ -1,11 +1,19 @@
 # 模型验证
-from ultralytics.models import YOLO
-from ultralytics import settings
-if __name__ == '__main__':
-    # Update a setting
-    settings.update({"datasets_dir": "D:\\study\\2025_gradproj\\Anti-UAV"})
-    model = YOLO("./runs/train_2025-02-05_08-29-53/weights/best.pt")
+from ultralytics import YOLO
 
-    # Validate with a custom dataset
-    metrics = model.val(data="./datasets/data.yaml")
-    print(metrics.box.map)  # map50-95
+import torch
+# 自动选择设备，GPU 优先，如果没有 GPU，则使用 CPU
+device = torch.device(0 if torch.cuda。is_available() else 'cpu')
+
+# Load a model
+
+model = YOLO("./runs/train_yoloi.yaml_2025-02-23_11-36-57/weights/best.pt")
+# model = YOLO("./runs/train_yolon.yaml_2025-03-03_08-03-43/weights/best.pt")
+
+# Customize validation settings
+validation_results = model.val(data="./datasets/data_val.yaml", imgsz=320, batch=64, conf=0.25, iou=0.6, device=device, project = "runs", name="val_yoloi")
+
+print(validation_results.box。map)  # mAP50-95
+print(validation_results.box。map50)  # mAP50
+print(validation_results.box。map75)  # mAP75
+print(validation_results.box。maps)  # list of mAP50-95 for each category
