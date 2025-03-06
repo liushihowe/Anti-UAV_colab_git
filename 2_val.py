6d0b32b548674a8a2ae330d934e016c23ae49771
liushihowe <whenwhenword@outlook.com>
2025-03-06 10:44:47 +0800

    #

--- 2_val.py
+++ 2_val.py
@@ -3,17 +3,17 @@ from ultralytics import YOLO
 
 import torch
 # 自动选择设备，GPU 优先，如果没有 GPU，则使用 CPU
-device = torch.device(0 if torch.cuda。is_available() else 'cpu')
+device = torch.device(0 if torch.cuda.is_available() else 'cpu')
 
 # Load a model
 
-model = YOLO("./runs/train_yoloi.yaml_2025-02-23_11-36-57/weights/best.pt")
+model = YOLO("./models/n_100.pt")
 # model = YOLO("./runs/train_yolon.yaml_2025-03-03_08-03-43/weights/best.pt")
 
 # Customize validation settings
-validation_results = model.val(data="./datasets/data_val.yaml", imgsz=320, batch=64, conf=0.25, iou=0.6, device=device, project = "runs", name="val_yoloi")
+validation_results = model.val(data="./models/data_val.yaml", imgsz=320, batch=64, conf=0.25, iou=0.6, device=device, project = "runs", name="val_yoloi")
 
-print(validation_results.box。map)  # mAP50-95
-print(validation_results.box。map50)  # mAP50
-print(validation_results.box。map75)  # mAP75
-print(validation_results.box。maps)  # list of mAP50-95 for each category
+# print(validation_results.box.map)  # mAP50-95
+print(f"map50是:{validation_results.box.map50}")  # mAP50
+# print(validation_results.box.map75)  # mAP75
+# print(validation_results.box.maps)  # list of mAP50-95 for each category
